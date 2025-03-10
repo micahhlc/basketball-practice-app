cb25dad772545ef5abcaa3d1db9f528614c66cff
Micah Cheng <micahhlc@gmail.com>
2025-03-11 01:10:14 +0900

    test tailwindcss

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,6 +1,23 @@
 <template>
-   <div>
-     <h1>Settings</h1>
-     <p>This is the Settings screen.</p>
-   </div>
- </template>
+  <div>
+    <h1>Settings</h1>
+    <p>This is the Settings screen.</p>
+  </div>
+  <div
+    class="rounded-md border border-white bg-red-500 px-4 py-2 font-bold text-white"
+  >
+    Prettier Test
+  </div>
+  <div
+    class="w-1/2 rounded-md border border-white bg-red-500 px-4 py-2 font-bold text-white"
+  >
+    Prettier Test
+  </div>
+  <div class="container m-4 w-1/2 space-y-4 bg-blue-100 p-2">
+    <div class="border-4 bg-orange-200">something inside</div>
+    <div class="border-4 bg-orange-200">something inside</div>
+    <div class="border-4 bg-orange-200">something inside</div>
+    <div class="border-4 bg-orange-200">something inside</div>
+    <div class="border-4 bg-orange-200">something inside</div>
+  </div>
+</template>
