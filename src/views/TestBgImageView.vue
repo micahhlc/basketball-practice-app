4e01135a91fc5e4e77ed69aec31b66fdaa792555
Micah Cheng <micahhlc@gmail.com>
2025-05-06 17:33:05 +0900

    update TestBgImageView

--- src/views/TestBgImageView.vue
+++ src/views/TestBgImageView.vue
@@ -3,10 +3,21 @@
   <div class="background-container">
     <!-- Each button is inside its own wrapper div -->
     <div class="button-wrapper button-one">
-      <button @click="handleClick1">Button 1</button>
+      <button
+        @click="handleClick1"
+        class="rounded-full p-2 transition-transform hover:scale-110"
+      >
+        <img src="../assets/basketball.png" alt="Basketball" class="h-8 w-8" />
+      </button>
     </div>
     <div class="button-wrapper button-two">
-      <button @click="handleClick2">Button 2</button>
+      <!-- <button @click="handleClick2">Button 2</button> -->
+      <button
+        @click="handleClick2"
+        class="rounded-full p-2 transition-transform hover:scale-110"
+      >
+        <img src="../assets/basketball.png" alt="Basketball" class="h-8 w-8" />
+      </button>
     </div>
   </div>
 </template>
@@ -29,8 +40,8 @@ export default {
 .background-container {
   position: relative;
   width: 100%;
-  height: 100vh;
-  background-image: url("../assets/HalfCourtBasketball.webp");
+  height: 40vh;
+  background-image: url("../assets/HalfCourtBasketball.png");
   background-size: cover;
   background-position: center;
 }
@@ -43,12 +54,12 @@ export default {
 /* Positioning for Button 1 */
 .button-one {
   top: 20%;
-  left: 35%;
+  left: 40%;
 }
 
 /* Positioning for Button 2 */
 .button-two {
-  top: 50%;
+  top: 35%;
   left: 70%;
 }
 
@@ -59,7 +70,6 @@ button {
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  background-color: #007bff;
   color: #fff;
 }
 </style>
